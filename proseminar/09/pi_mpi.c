b55012df66267458e7a65521fac5c80f5c42ba9d
MunsMan <munsman.github@gmail.com>
2023-12-04 12:12:31 +0100

    pi mpi

---
+++ proseminar/09/pi_mpi.c
@@ -0,0 +1,66 @@
+#include "xoshiro256plus.c"
+#include <mpi.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(int argc, char* argv[]) {
+	if(argc < 2) {
+		printf("Usage: %s <N>\n", argv[0]);
+		return EXIT_FAILURE;
+	}
+	uint64_t N = strtoul(argv[1], NULL, 10);
+
+	int myRank, numProcs;
+
+	MPI_Init(&argc, &argv);
+
+	MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
+	MPI_Comm_size(MPI_COMM_WORLD, &numProcs);
+
+	// Ensure that work is evenly distributed
+	// allows integer divisions
+	// could be easily fixed
+	if(N % numProcs != 0) {
+		MPI_Finalize();
+		return EXIT_FAILURE;
+	}
+	uint64_t inside = 0;
+	uint64_t timeSeed = time(NULL) + myRank;
+	double starttime, endtime;
+	if(myRank == 0) {
+
+		starttime = MPI_Wtime();
+
+		s[0] = timeSeed;
+		s[1] = timeSeed ^ (timeSeed >> 32);
+		s[2] = ~timeSeed;
+		s[3] = timeSeed << 32;
+
+		uint64_t upper_bound = UINT64_MAX >> 2;
+		uint64_t maks = UINT32_MAX >> 1;
+
+		for(uint64_t i = 0; i < N / numProcs; i++) {
+			uint64_t r = next();
+			uint64_t x = r >> 33;
+			uint64_t y = r & maks;
+			if(x * x + y * y <= upper_bound) {
+				inside++;
+			}
+		}
+	}
+
+	uint64_t total_inside;
+	MPI_Reduce(&inside, &total_inside, 1, MPI_UINT64_T, MPI_SUM, 0, MPI_COMM_WORLD);
+
+	if(myRank == 0) {
+
+		endtime = MPI_Wtime();
+		printf("Wall: %f seconds\n", endtime - starttime);
+		printf("Pi approximately: %f\n", 4.0 * (double)total_inside / (double)N);
+	}
+
+	MPI_Finalize();
+	return EXIT_SUCCESS;
+}
